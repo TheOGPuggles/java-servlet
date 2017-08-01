12b156047cc248ce7ec2a62f1393cebbed35e450
triggerman722 <gemartin@gmail.com>
2017-07-31 21:24:53 -0400

    Create Conn.java

---
+++ src/main/java/thing/controller/Conn.java
@@ -0,0 +1,16 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+public class Conn {
+
+   private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+}
