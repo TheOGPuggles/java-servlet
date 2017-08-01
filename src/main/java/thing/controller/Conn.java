91c0960f7c73c3a41000b090eb155aa918f24f0b
triggerman722 <gemartin@gmail.com>
2017-07-31 21:28:14 -0400

    Update Conn.java

--- src/main/java/thing/controller/Conn.java
+++ src/main/java/thing/controller/Conn.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class Conn {
 
-   private static Connection getConnection() throws URISyntaxException, SQLException {
+   public static Connection getConnection() throws URISyntaxException, SQLException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
