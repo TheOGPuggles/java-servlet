f17452912ff208a65df8f7b04a7f91f4232eb154
triggerman722 <gemartin@gmail.com>
2017-07-31 21:26:18 -0400

    Update JunkServlet.java

--- src/main/java/thing/controller/JunkServlet.java
+++ src/main/java/thing/controller/JunkServlet.java
@@ -4,12 +4,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
 
 @WebServlet(value="/junk")
 public class JunkServlet extends HttpServlet {
      @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
+        Connection connection = Conn.getConnection();
+        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        while (rs.next()) {
+            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+        }
         response.getWriter().println("Junk Hello world!!");
     }
 }
